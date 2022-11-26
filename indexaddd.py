ae94d6153ba2bb042063bc2c96904f8b6a82b660
Sjk1001 <sjk1001@hotmail.com>
2022-11-26 21:53:37 +0900

    text type

--- indexaddd.py
+++ indexaddd.py
@@ -22,5 +22,42 @@ result = num1 + num2
 # [num = num + 1]
 num /=5
 
+# 整数を代入する場合
+num = 10
+print(type(num))
+# 小数を代入する場合
+num = 33.14
+print(type(num))
 
+#single quatation ''
+#double quatation ""
+text = 'apple'
+print(text)
+print(type(text))
 
+# 文字列の連携
+text = 'Hello' + ',' +'Aidemyy'
+print(text)
+
+# 変数に文字入れ
+text2 = 'Python'
+text = 'Hello' + ', ' + text2
+print(text)
+
+# 文字繰り返す
+text = 'Hello' * 4
+print(text)
+text = 'Hello'
+text += ',World'
+print(text)
+text1 = '1111'
+text2 = '2222'
+print(text1+text2)
+
+# bool Type
+data1 = True
+print(data1)
+print(type(data1))
+data2 = False
+print(data2)
+print(type(data2))
