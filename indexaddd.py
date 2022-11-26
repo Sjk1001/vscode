c2b58fcbf5017ef9390d5bf0370e8046a6e5f1bb
Sjk1001 <sjk1001@hotmail.com>
2022-11-26 21:15:14 +0900

    変数

--- indexaddd.py
+++ indexaddd.py
@@ -1,1 +1,26 @@
 print('Hello world')
+
+# 変数
+num = 10
+print(num)
+
+# 変数の上書き
+num = 10
+num = 30
+
+# 変数の代入
+num = 10
+copy_num  = num
+print(copy_num)
+
+# 変数の計算
+num1 = 10
+num2 = 5
+result = num1 + num2
+
+#同じ変数に対して計算を行う
+# [num = num + 1]
+num /=5
+
+
+
