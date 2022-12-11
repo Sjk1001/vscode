a98a9cb78f21d9de02151e282bacd2a97d0a0eea
Sjk1001 <sjk1001@hotmail.com>
2022-12-12 00:52:04 +0900

    2.1.4 グラフにグリッドを表示する

--- Matplotlib/Matplotlib_2-1-4.py
+++ Matplotlib/Matplotlib_2-1-4.py
@@ -1,2 +1,17 @@
 #2.1.4 グラフにグリッド（目盛線）を表示する
 print('--------------2.1.4 グラフにグリッドを表示する--------------')
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, np.pi)
+y = np.cos(x)
+
+plt.title("cos Graph")
+plt.xlabel("x-axis")
+plt.xlim([0, 3])
+
+#グラフにグリッドを表示
+plt.grid(True)
+
+plt.plot(x, y)
+plt.show()
