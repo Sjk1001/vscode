0d4405175e2dc44fb6855947b172ab4d5a010575
Sjk1001 <sjk1001@hotmail.com>
2022-12-13 23:49:01 +0900

    週末復讐

--- Pandas/Pandas_base.py
+++ Pandas/Pandas_base.py
@@ -333,3 +333,240 @@ df_data2 = make_random_df(range(1, 5), columns2, 1)
 # df_data1とdf_data2を縦方向に連結しdf1に代入
 df1 = pd.concat([df_data1, df_data2], axis=0)
 print(df1)
+
+
+#2.2.3 連結する際のラベルの指定
+print('------------------2.2.3 連結する際のラベルの指定-------------------')
+import numpy as np
+import pandas as pd
+
+# 指定のインデックスとカラムを持つDataFrameを乱数によって作成する関数
+def make_random_df(index, columns, seed):
+    np.random.seed(seed)
+    df = pd.DataFrame()
+    for column in columns:
+        df[column] = np.random.choice(range(1, 101), len(index))
+    df.index = index
+    return df
+
+columns = ["apple", "orange", "banana"]
+df_data1 = make_random_df(range(1, 5), columns, 0)
+df_data2 = make_random_df(range(1, 5), columns, 1)
+
+# df_data1とdf_data2を横方向に連結し、keysに"X", "Y"を指定してMultiIndexにしてdfに代入してください
+df = pd.concat([df_data1, df_data2], axis=1, keys= ["X", "Y"])
+
+# dfの"Y"ラベルの"banana"をY_bananaに代入してください。
+Y_banana = df["Y","banana"]
+
+print(df)
+print()
+print(Y_banana)
+
+
+#2.3.2 内部外部結合の基本--inner--outer
+print('----------------------2.3.2 内部外部結合の基本---------------------')
+import pandas as pd
+
+data1 = {"fruits": ["apple", "orange", "banana", "strawberry", "kiwifruit"],
+        "year": [2001, 2002, 2001, 2008, 2006],
+        "amount": [1, 4, 5, 6, 3]}
+df1 = pd.DataFrame(data1)
+
+data2 = {"fruits": ["apple", "orange", "banana", "strawberry", "mango"],
+        "year": [2001, 2002, 2001, 2008, 2007],
+        "price": [150, 120, 100, 250, 3000]}
+df2 = pd.DataFrame(data2)
+
+# df1, df2のデータを確認してください
+print(df1)
+print()
+print(df2)
+print()
+
+# df1とdf2を"fruits"をKeyに内部結合して作成したDataFrameをdf3に代入してください
+df3 = pd.merge(df1, df2, on= "fruits", how="inner")
+
+# 内部結合を行った時の挙動を確認しましょう
+print(df3)
+
+
+#2.3.4 同名でない列をKeyにして結合する
+print('---------------2.3.4 同名でない列をKeyにして結合する----------------')
+import pandas as pd
+
+# 注文情報
+order_df = pd.DataFrame([[1000, 2546, 103],
+                         [1001, 4352, 101],
+                         [1002, 342, 101],
+                         [1003, 1192, 102]],
+                         columns=["id", "item_id", "customer_id"])
+# 顧客情報
+customer_df = pd.DataFrame([[101, "Tanaka"],
+                           [102, "Suzuki"],
+                           [103, "Kato"]],
+                           columns=["id", "name"])
+
+# order_dfとcustomer_dfを顧客IDをkeyに結合してorder_dfに代入してください
+order_df = pd.merge(order_df, customer_df, 
+                        left_on= "customer_id", right_on="id", how="inner" )
+
+print(order_df)
+
+
+#2.3.5 インデックスをKeyにして結合する
+print('---------------2.3.5 インデックスをKeyにして結合する----------------')
+import pandas as pd
+
+# 注文情報
+order_df = pd.DataFrame([[1000, 2546, 103],
+                         [1001, 4352, 101],
+                         [1002, 342, 101],
+                         [1003, 1192, 102]],
+                         columns=["id", "item_id", "customer_id"])
+# 顧客情報
+customer_df = pd.DataFrame([["Tanaka"],
+                           ["Suzuki"],
+                           ["Kato"]],
+                           columns=["name"])
+customer_df.index = [101, 102, 103]
+
+# order_dfとcustomer_dfを顧客IDをkeyに結合してorder_dfに代入してください
+order_df = pd.merge(order_df, customer_df, left_on="customer_id", right_index= True, how= "inner")
+
+print(order_df)
+
+
+#2.4.1 一部の行を得る
+print('------------------------2.4.1 一部の行を得る------------------------')
+import numpy as np
+import pandas as pd
+
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+
+# DataFrameを生成し、列を追加
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+
+# dfの冒頭3行を取得し、df_headに代入してください
+df_head = df.head(3)
+
+# dfの末尾3行を取得し、df_tailに代入してください
+df_tail = df.tail(3)
+
+print(df_head)
+print()
+print(df_tail)
+
+
+#2.4.2 計算処理を適用する
+print('----------------------2.4.2 計算処理を適用する----------------------')
+import pandas as pd
+import numpy as np
+
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+
+# DataFrameを生成し、列を追加
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+print(df)
+# dfの各要素に+2、ブロードキャストによりPandasと整数の計算ができます
+add_df = df+2
+print(add_df)
+
+import numpy as np
+import pandas as pd
+
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+
+# DataFrameを生成し、列を追加
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+
+# dfの各要素を2倍し、double_dfに代入してください
+double_df = df * 2
+
+# dfの各要素を2乗し、square_dfに代入してください
+square_df = df ** 2
+
+# dfの各要素の平方根を計算し、sqrt_dfに代入してください
+sqrt_df = np.sqrt(df)
+
+print(double_df)
+print()
+print(square_df)
+print()
+print(sqrt_df)
+
+
+#2.4.3 要約統計量を得る
+print('----------------------2.4.3 要約統計量を得る-----------------------')
+import numpy as np
+import pandas as pd
+
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+
+# DataFrameを生成し、それぞれのcolumnに対応する値（要素）を10個追加する列を追加
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+
+# dfの要約統計量のうち、"mean", "max", "min"を取り出してdf_desに代入してください
+df_des = df.describe().loc[["mean", "max", "min"]]
+
+print(df_des)
+
+
+#2.4.4 DataFrameの行間または列間の差を求める
+print('-------------2.4.4 DataFrameの行間または列間の差を求める------------')
+import numpy as np
+import pandas as pd
+
+np.random.seed(0)
+columns = ["apple", "orange", "banana", "strawberry", "kiwifruit"]
+
+# DataFrameを生成し、列を追加
+df = pd.DataFrame()
+for column in columns:
+    df[column] = np.random.choice(range(1, 11), 10)
+df.index = range(1, 11)
+
+# dfの各行について、2行後の行との差を計算したDataFrameをdf_diffに代入してください
+df_diff = df.diff(periods=-2, axis=0)
+
+# dfとdf_diffのデータを比較して処理内容を確認してください
+print(df)
+print()
+print(df_diff)
+
+
+#2.4.5 グループ化
+print('-------------------------2.4.5 グループ化--------------------------')
+import pandas as pd
+
+# 一部の都道府県に関するDataFrameを作成
+prefecture_df = pd.DataFrame([["Tokyo", 2190, 13636, "Kanto"], ["Kanagawa", 2415, 9145, "Kanto"],
+                              ["Osaka", 1904, 8837, "Kinki"], ["Kyoto", 4610, 2605, "Kinki"],
+                              ["Aichi", 5172, 7505, "Chubu"]], 
+                             columns=["Prefecture", "Area", "Population", "Region"])
+
+# prefecture_dfを地域(Region)についてグループ化し、grouped_regionに代入してください
+grouped_region = prefecture_df.groupby("Region")
+
+# grouped_regionに出てきた地域ごとの、面積(Area)と人口(Population)の平均をmean_dfに代入してください
+mean_df = grouped_region.mean()
+
+print(prefecture_df)
+print()
+print(mean_df)
